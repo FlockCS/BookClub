92ce3e61e808486e374328be0334e06a146061f0
Manny6902 <mpylla@purdue.edu>
2025-06-21 15:58:51 -0400

    Create seperate Alpha and Prod bots for testing and live

--- lib/book-club-bot-stack.ts
+++ lib/book-club-bot-stack.ts
@@ -1,29 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
 
+export interface BookClubBotStackProps extends cdk.StackProps {
+  stage: string;
+  discordPublicKey: string;
+}
+
 export class BookClubBotStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: BookClubBotStackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'BookClubBotQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
 
     const dockerFunction = new lambda.DockerImageFunction(
       this,
-      "DockerFunction",
+      `${props.stage}DockerFunction`,
       {
         code: lambda.DockerImageCode.fromImageAsset("./src"),
         memorySize: 1024,
         timeout: cdk.Duration.seconds(10),
         architecture: lambda.Architecture.X86_64,
         environment: {
-          DISCORD_PUBLIC_KEY: "2250998ebe1d6f565a07459e0263009cc18c482351e53c3415921362e69f34d6",
+          DISCORD_PUBLIC_KEY: props.discordPublicKey,
         },
       }
     );
@@ -37,7 +34,7 @@ export class BookClubBotStack extends cdk.Stack {
       },
     });
 
-    new cdk.CfnOutput(this, "FunctionUrl", {
+    new cdk.CfnOutput(this, `${props.stage}FunctionUrl`, {
       value: functionUrl.url,
     });
 
