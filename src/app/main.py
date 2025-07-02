e290c01b499969f7a98b94998e9dabb2a979a6de
Maanas <mm.pimplikar@gmail.com>
2025-07-01 22:50:07 -0700

    updated import

--- src/app/main.py
+++ src/app/main.py
@@ -5,7 +5,7 @@ from discord_interactions import verify_key_decorator
 from helper_functions import handle_book_select, handle_schedule_select
 from command_handler import command_handler
 from config import DISCORD_PUBLIC_KEY, IN_DEVELOPMENT
-from utils.aws.elasticache import test_function
+from app.utils.aws.elasticache import test_function
 
 # @TODO: Convert this to use redis instead
 # pending selections
