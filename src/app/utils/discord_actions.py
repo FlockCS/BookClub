851ec71935656586e6fd89025b038718a1e2dfcf
prabhav-pande <prabhav2833@gmail.com>
2025-10-09 20:30:42 -0400

    Change from @everyone to @Reader

--- src/app/utils/discord_actions.py
+++ src/app/utils/discord_actions.py
@@ -127,7 +127,7 @@ def create_event_announcement(guild_id, payload):
         raise ValueError("Announcements channel not found in guild")
 
     url = f"{DISCORD_API_BASE}/channels/{channel_id}/messages"
-    message_content = f"@everyone\n\n{hf_query(payload)}"
+    message_content = f"@Reader\n\n{hf_query(payload)}"
     # message_content = f"{hf_query(payload)}"
     hf_response = {"content": message_content}
 
