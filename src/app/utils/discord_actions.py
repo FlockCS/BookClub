53c894b827a588093bd711dbcbd240c3fbf4e4e0
prabhav-pande <prabhav2833@gmail.com>
2025-10-11 01:35:55 -0400

    Reminders working

--- src/app/utils/discord_actions.py
+++ src/app/utils/discord_actions.py
@@ -158,8 +158,7 @@ def create_event_announcement(guild_id, payload):
     url = f"{DISCORD_API_BASE}/channels/{channel_id}/messages"
     READER_ROLE_ID = "1394431409191387156"
     if ENVIRONMENT == "PROD":
-        # message_content = f"<@&{READER_ROLE_ID}>\n\n{hf_query(payload)}"
-        message_content = f"{hf_query(payload)}"
+        message_content = f"<@&{READER_ROLE_ID}>\n\n{hf_query(payload)}"
     else:
         message_content = f"{hf_query(payload)}"
     hf_response = {"content": message_content}
